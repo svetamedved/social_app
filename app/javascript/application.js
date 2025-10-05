778e38d6becf2130ac11377bf206f00ac6fdc9a6
svetamedved <sveta.berrios@gmail.com>
2025-10-04 22:17:56 -0500

    Manually add Rails UJS for logout functionality

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,5 @@
+import Rails from "@rails/ujs"
+Rails.start()
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
